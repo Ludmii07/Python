bc2b713ecef5058f40c6e967a9272af478baa23b
Ludmii07 <134977183+Ludmii07@users.noreply.github.com>
2023-06-19 19:37:17 -0300

    Add files via upload

---
+++ 17_ejemplo1_funciones.py
@@ -0,0 +1,9 @@
+# Codificar una función que imprima el saludo "Hola! Como estas?" cada vez que se invoque.
+
+def saludar ():
+    print ("\nHola! Como estas?\n") # \n => salto de linea
+
+saludar () # Ejecuta la funcion
+
+for i in range (3): # Para ejecutarlo varias veces
+    saludar() 
